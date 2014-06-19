eb35c441306c16aebba7fd73c8524eaffb0a4e1a
Naman Goel <naman34@gmail.com>
2014-06-19 04:21:55 -0700

    added complete API for querying db

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -5,20 +5,43 @@ var router = express.Router();
 
 
 module.exports = function(app){
-  // var Vehicle;
+  var Vehicle;
   
-  // function setModel(){
-  //   Vehicle = Vehicle || app.get('Vehicle');
-  // }
+  function setModel(){
+    Vehicle = Vehicle || app.get('Vehicle');
+  }
 
   router.get('/', function(req, res) {
-    var Vehicle = app.get('Vehicle');
-    Vehicle.find().then(res.send.bind(res));
+    setModel();
+    var findObj = {};
+    if(!!req.query.where){
+      try {
+        findObj = JSON.parse(req.query.where);
+      } catch(e){
+        return res.send(500, "Invalid Query parameter");
+      }
+    }
+
+    var query = Vehicle.find(findObj);
+
+    if(!!req.query.skip){
+      query = query.skip(parseInt(req.query.skip));
+    }
+
+    if(!!req.query.limit){
+      query = query.limit(parseInt(req.query.limit));
+    }
+
+    if(!!req.query.sort){
+      query = query.sort(JSON.parse(req.query.sort));
+    }
+
+    query.then(res.send.bind(res));
   });
 
   router.get('/:id', function(req, res) {
-    // setModel();
-    var Vehicle = app.get('Vehicle');
+    setModel();
+    //var Vehicle = app.get('Vehicle');
     Vehicle.findOne({id:req.params.id}).then(res.send.bind(res));
   });
 
