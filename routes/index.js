28eee21154fddd2e8f2496daa15dee7830f32b2b
Naman Goel <naman34@gmail.com>
2014-06-19 18:01:19 -0700

    added react for the search

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,15 @@
+var React = require('react');
+var search = require('./../ReactComponents/search');
+
+var x = React.createClass({
+  render: function(){
+    return React.DOM.div({}, 'something');
+  }
+});
+
 module.exports = function(req, res) {
-  res.render('index', { title: 'Instamotor' });
+  res.render('index', { 
+    title: 'Instamotor', // Only for the page title
+    ReactComponent: React.renderComponentToString(search({title: 'Instamotor'}))
+  });
 };
